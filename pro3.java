fe35e5b343100f92c966ad47ead68dab6c62d361
Hari <19am1a0510@svrec.ac.in>
2022-05-02 19:04:16 +0530

    first commit

---
+++ pro3.java
@@ -0,0 +1,9 @@
+class Details
+{
+    public static void main(String args[])
+    {
+        System.out.println("Name : Haritha");
+        System.out.println("Branch : CSE");
+        System.out.println("year : 3");
+    }
+}
